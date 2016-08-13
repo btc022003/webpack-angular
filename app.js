02390b8fea29f46255697dc72d90a82db2339249
yuluo_lp <btc022003@gmail.com>
2016-08-13 22:56:34 +0800

    初始化基础引用

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+
 var app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+
+
+app.listen(3000,function(){
+  console.log('服务器运行中....')
+})
