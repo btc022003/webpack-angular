b43906cd4d4c1dcfc8c33a1476ca670d94a5d66c
yuluo_lp <btc022003@gmail.com>
2016-08-21 21:58:55 +0800

    --标准人员信息接口数据代码完善

--- routes/api/v1/persons.js
+++ routes/api/v1/persons.js
@@ -5,6 +5,41 @@ var express = require('express');
 var router = express.Router();
 
 
+router.get('/', (req, res) => {
+    res.json({
+        status: 'y',
+        data: [],
+        msg: '获取所有人员信息成功'
+    })
+})
 
+router.get('/:id', (req, res) => {
+    res.json({
+        status: 'y',
+        data: {},
+        msg: '获取人员信息成功'
+    })
+})
+
+router.post('/new',(req,res)=>{
+  res.json({
+    status: 'y',
+    msg: '新增人员信息成功'
+  })
+})
+
+router.put('/update/:id',(req,res)=>{
+  res.json({
+    status: 'y',
+    msg: '修改人员信息成功'
+  })
+})
+
+router.delete('/:id',(req,res)=>{
+  res.json({
+    status: 'y',
+    msg: '删除人员信息成功'
+  })
+})
 
 module.exports = router;
