47d7bc68fa913c3519c3eb754bd84e755d590ffd
yuluo_lp <btc022003@gmail.com>
2016-09-07 20:31:16 +0800

    此方法貌似不行 待换思路

--- app.js
+++ app.js
@@ -19,13 +19,20 @@ app.use(express.static('bundle'))
  * 加入api调用时跨域访问问题的处理
  */
 app.all('/api/*', (req, res, next) => {
+  console.log(req.method)
+
   console.log('请求已到达...')
     res.header('Access-Control-Allow_Origin', '*')
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
     res.header("X-Powered-By", ' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8")
-    next()
+    if(req.method == "OPTIONS"){
+      res.send('')
+    }
+    else{
+      res.header("Content-Type", "application/json;charset=utf-8")
+        next()
+    }
 })
 
 ////引入api的js
